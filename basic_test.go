d1987fd0f461677e14fe3213826c0ef20da87d38
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:02 -0800

    tests

--- basic_test.go
+++ basic_test.go
@@ -3,15 +3,14 @@ package shuf_test
 import (
 	"strings"
 
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/shuf"
 )
 
 func ExampleShuf_basic() {
 	// echo "1\n2\n3\n4\n5" | shuf -n 2
 	// Note: Output is random, so we just demonstrate usage
-	yup.MustRun(
+	gloo.MustRun(
 		Shuf(Count(2), strings.NewReader("1\n2\n3\n4\n5")),
 	)
 }
-
